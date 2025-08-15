4c13c39b873b73bfffac52ebc608ccbdbe345762
ajay93015 <105852839+ajay93015@users.noreply.github.com>
2025-08-15 19:22:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-.
+const app = require("express")();
+app.use(require("express").static("public"))
+    .set('view engine', 'ejs')
+    .set('views', __dirname + '/views');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+const renderPage = (page, options = {}) => (req, res) => {
+ // const user = req.session?.user || undefined;
+  res.render(page, {
+    ...options,
+ //   user // pass user to template
+  });
+};
+
+app.get('/',renderPage('int'));
+app.get('/login',renderPage('log'));
+
+app.listen(80);
