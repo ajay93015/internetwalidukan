6b567d6663de0d69b184247ebdf4732f06833d6e
ajay93015 <105852839+ajay93015@users.noreply.github.com>
2025-12-15 18:14:14 +0530

    vercel

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.use((req,res,next)=>{
     `);
 })
 
-module.exports = app;
-/*
+//module.exports = app;
+
   app.listen(
   { port: process.env.PORT, host: "0.0.0.0" },
   function (err, address) {
@@ -45,7 +45,8 @@ module.exports = app;
   }
 );
 
-*/
+
+
 
 
 
